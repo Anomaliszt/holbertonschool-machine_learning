fa40e67821d185e2d695e7e6cf3a26355e2e242b
Xavier Sanchez <xavier.sanchez44@orange.fr>
2026-07-07 11:23:44 +0200

    Update 3-pool_backward.py

--- supervised_learning/cnn/3-pool_backward.py
+++ supervised_learning/cnn/3-pool_backward.py
@@ -1,1 +1,36 @@
-s
+#!/usr/bin/env python3
+import numpy as np
+
+
+def pool_backward(dA, A_prev, kernel_shape, stride=(1, 1), mode='max'):
+    """Performs back propagation over a pooling layer of a neural network."""
+    
+    m, h_prev, w_prev, c_prev = A_prev.shape
+    kh, kw = kernel_shape
+    sh, sw = stride
+    m, h_new, w_new, c = dA.shape
+
+    # Initialize gradient
+    dA_prev = np.zeros_like(A_prev)
+
+    for i in range(m):
+        for h in range(h_new):
+            for w in range(w_new):
+                for c in range(c_prev):
+                    vert_start = h * sh
+                    vert_end = vert_start + kh
+                    horiz_start = w * sw
+                    horiz_end = horiz_start + kw
+
+                    if mode == 'max':
+                        # Find the position of the max value in the window
+                        a_prev_slice = A_prev[i, vert_start:vert_end, horiz_start:horiz_end, c]
+                        mask = (a_prev_slice == np.max(a_prev_slice))
+                        dA_prev[i, vert_start:vert_end, horiz_start:horiz_end, c] += mask * dA[i, h, w, c]
+
+                    elif mode == 'avg':
+                        # Distribute the gradient equally across the window
+                        da = dA[i, h, w, c]
+                        dA_prev[i, vert_start:vert_end, horiz_start:horiz_end, c] += (da / (kh * kw))
+
+    return dA_prev
